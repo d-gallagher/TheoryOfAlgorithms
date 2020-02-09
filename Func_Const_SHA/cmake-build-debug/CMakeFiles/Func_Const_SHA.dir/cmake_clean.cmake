fbaeaf66d751bbac80bdc180f26c1a1f744b2efc
d-gallagher <jibisjib@gmail.com>
2020-02-09 18:20:32 +0000

    Updated project structure for easier CLion IDE Development

---
+++ Func_Const_SHA/cmake-build-debug/CMakeFiles/Func_Const_SHA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Func_Const_SHA.dir/sha256.c.obj"
+  "Func_Const_SHA.exe"
+  "Func_Const_SHA.exe.manifest"
+  "Func_Const_SHA.lib"
+  "Func_Const_SHA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Func_Const_SHA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
