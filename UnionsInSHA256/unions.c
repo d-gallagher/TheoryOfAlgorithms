80298a78cdab605a052dd78d83c4f1dfe8f26c21
d-gallagher <jibisjib@gmail.com>
2020-03-01 14:03:34 +0000

    Updated unions to look at endianness in 18 and 8 bit unsigned ints

--- UnionsInSHA256/unions.c
+++ UnionsInSHA256/unions.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <inttypes.h>
 
 
+union endian{
+  uint8_t  ei[2];
+  uint16_t si;
+};
 
 
 int main(int argc, char *argv[]) {
-    printf("Hello, World!\n");
+
+    union endian myunion;
+    myunion.si = 0xff;
+    myunion.ei[0] = 0x2;
+    myunion.ei[1] = 0xab;
+
+
+    // Stored in big endian (on this  machine)
+    printf("16bit: %04" PRIx16 "\n", myunion.si);
+    // Stored as little endian
+    printf(" 8bit: %02" PRIx8 " %02" PRIx8 "\n", myunion.ei[0], myunion.ei[1]);
     return 0;
 }
