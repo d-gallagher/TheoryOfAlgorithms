4eccc5562aeef6c329bf5dab0fd6c8ec5009c26e
d-gallagher <jibisjib@gmail.com>
2020-03-01 14:24:56 +0000

    Updated padding to use union block

--- Padding/padding.c
+++ Padding/padding.c
@@ -5,6 +5,14 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+// Represents the currrent block which has been read from the padded message
+// Can be read as an array of 64, 32 or 8 bit integers
+union block{
+    uint64_t sixfour[8];
+    uint32_t threetwo[16];
+    uint8_t eight[64];
+};
+
 // nobits is the number of bits we read form the file (reading file in blocks of x size)
 // If numbits mod 512 (ULL = Unsingned long long) has a remainder, this is where we start padding.
 // Hence the 512ULL minus the modulo of nobits to the 512.
@@ -42,13 +50,15 @@ int main(int argc, char *argv[]){
     }
 
     // Bitesize to read
-    uint8_t _bits;
+    uint8_t _bits; // This is no longer used and is now represented by the M.eight element of the union block
+    union block M;
     uint64_t _nobits;
+    uint8_t i;
 
     // Read the file one byte at a time
     // PTIx8 is using the inttypes lib to print the uint as hex
-    for (_nobits = 0; fread(&_bits, 1, 1, infile) == 1; _nobits += 8) {
-        printf("%02" PRIx8, _bits);
+    for (_nobits = 0, i = 0; fread(&M.eight[i], 1, 1, infile) == 1; _nobits += 8) {
+        printf("%02" PRIx8, M.eight[i]);
     }
 
     // Print bits of hex value 80 => 1000 0000 (this padding will always be required in sha256)
