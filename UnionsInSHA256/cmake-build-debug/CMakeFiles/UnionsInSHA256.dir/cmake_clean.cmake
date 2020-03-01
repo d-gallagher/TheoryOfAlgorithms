c5704a37ac9014bb48acab910911e78df4722017
d-gallagher <jibisjib@gmail.com>
2020-03-01 13:22:43 +0000

    Unions in c for sha256

---
+++ UnionsInSHA256/cmake-build-debug/CMakeFiles/UnionsInSHA256.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UnionsInSHA256.dir/unions.c.obj"
+  "UnionsInSHA256.exe"
+  "UnionsInSHA256.exe.manifest"
+  "UnionsInSHA256.lib"
+  "UnionsInSHA256.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/UnionsInSHA256.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
