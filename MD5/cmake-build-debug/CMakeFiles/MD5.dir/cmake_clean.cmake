dda721e12349206fb372f9442cc0eff76cb7e9ba
d-gallagher <jibisjib@gmail.com>
2020-03-15 17:19:51 +0000

    Init MD5 assignment in CLion - C99

---
+++ MD5/cmake-build-debug/CMakeFiles/MD5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MD5.dir/main.c.obj"
+  "MD5.exe"
+  "MD5.exe.manifest"
+  "MD5.lib"
+  "MD5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MD5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
