dcc3798f79ecd054fd5359dcdad27d8eba1bf906
d-gallagher <jibisjib@gmail.com>
2020-03-20 14:45:46 +0000

    Added functions for rounds 1-4. Union Block. Helper Functions for endianness

--- MD5/main.c
+++ MD5/main.c
@@ -8,7 +8,7 @@
 // word as 32 bit integer
 #define WORD uint32_t
 // Rotate Left
-#define ROTL(x, n) (((x) << (n)) | ((x) >> (32 - (n))))
+#define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32 - (n))))
 
 #define F(x, y, z) ((x & y) | (~x & z))
 
@@ -17,6 +17,46 @@
 #define H(x, y, z) (x ^ y ^ z)
 
 #define I(x, y, z) (y ^ (x | ~z))
+
+// Processing functions for:
+// Round 1
+void FF(WORD *a, WORD b, WORD c, WORD d, WORD x, WORD s, WORD ac){
+    *a += F(b, c, d) + x + ac;
+    *a = b + ROTATE_LEFT(*a, s);
+}
+// Round 2
+void GG(WORD *a, WORD b, WORD c, WORD d, WORD x, WORD s, WORD ac){
+*a += G(b, c, d) + x + ac;
+*a = b + ROTATE_LEFT(*a, s);
+}
+// Round 3
+void HH(WORD *a, WORD b, WORD c, WORD d, WORD x, WORD s, WORD ac){
+    *a += H(b, c, d) + x + ac;
+    *a = b + ROTATE_LEFT(*a, s);
+}
+// Round 4
+void II(WORD *a, WORD b, WORD c, WORD d, WORD x, WORD s, WORD ac){
+    *a += I(b, c, d) + x + ac;
+    *a = b + ROTATE_LEFT(*a, s);
+}
+
+// A sixty-four byte block of memory, accessed with different types.
+// Represents the current block which has been read from the padded message
+// The Union will consume 64 bytes of memory
+// Can be read as an array of 64, 32 or 8 bit integers
+typedef union
+{
+    uint64_t sixfour[8];
+    WORD threetwo[16];
+    uint8_t eight[64];
+} BLOCK;
+
+// Flags represent the four different states that next block may encounter:
+// READ   - Still reading file
+// PAD0   - (Not enough space to complete the padding in the current block but the 1 bit has been appended already)
+// FINISH - Padding is complete.
+typedef enum {READ, PAD0, FINISH} PADFLAG;
+
 // Check endianness of machine
 int is_big_endian(void)
 {
@@ -26,6 +66,27 @@ int is_big_endian(void)
     } e = { 0x01000000 };
 
     return e.c[0];
+}
+
+// MD5 specifies big endian byte order, but this implementation assumes a little
+// endian byte order CPU. swap_endianness will swap from big-little or little-big
+uint64_t  swap_endianness(uint64_t x){
+
+    uint64_t mask[8];
+    mask[0] = 0xff;
+    for (int i = 1; i < 8; i++) {
+        mask[i] = mask[0] << (8 * i);
+    }
+    uint64_t y =    (x >> 56) & mask[0]
+                    ^ ((x >> 40) & mask[1])
+                    ^ ((x >> 24) & mask[2])
+                    ^ ((x >>  8) & mask[3])
+                    ^ ((x <<  8) & mask[4])
+                    ^ ((x << 24) & mask[5])
+                    ^ ((x << 40) & mask[6])
+                    ^ ((x << 56) & mask[7]);
+
+    return y;
 }
 
 int main(int argc, char *argv[]) {
